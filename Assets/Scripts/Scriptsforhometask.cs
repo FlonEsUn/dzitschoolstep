a2e7b6278eef68f2da00a43c55fdb15c2ff419b4
FlonEsUn <Past_uu72@student.itstep.org>
2024-04-25 22:22:41 +0300

    sdf

--- Assets/Scripts/Scriptsforhometask.cs
+++ Assets/Scripts/Scriptsforhometask.cs
@@ -4,29 +4,15 @@ public class Example : MonoBehaviour
 {
     public Camera camera1;
     public Transform cubeTransform;
-    public Light light2;
     public Vector3 vector3;
-    public Camera camera2;
-    public Camera camera3;
 
     void Start()
     {
         // 1-й елемент:
-        camera1.fieldOfView = 60; // Змінити FOV
+        camera1.fieldOfView = 100; // Змінити FOV
         camera1.backgroundColor = Color.black; // Змінити background
 
-        cubeTransform.localScale = new Vector3(1, 1, 1); // Змінити розмір куба
+        cubeTransform.localScale = new Vector3(1, 10, 1); // Змінити розмір куба
         cubeTransform.position = new Vector3(0, 0, 0); // Змінити позицію куба
-
-        // 2-й елемент:
-        light2.intensity = 1; // Змінити intensity
-        light2.color = Color.white; // Змінити color
-
-        vector3 = new Vector3(20, 24.77f, 89.871f); // Задати вектор
-
-        // 3-й елемент:
-        camera1.enabled = false; // Виключити першу камеру
-        camera2.enabled = false; // Виключити другу камеру
-        camera3.enabled = true; // Ввімкнути третю камеру
     }
 }
