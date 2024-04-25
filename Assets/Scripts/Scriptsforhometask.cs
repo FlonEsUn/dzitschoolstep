9ebfc2c1d11d0989100bdded1e2641198268c2d8
FlonEsUn <Past_uu72@student.itstep.org>
2024-04-25 22:55:26 +0300

    last

--- Assets/Scripts/Scriptsforhometask.cs
+++ Assets/Scripts/Scriptsforhometask.cs
@@ -4,15 +4,28 @@ public class Example : MonoBehaviour
 {
     public Camera camera1;
     public Transform cubeTransform;
+    public Light light2;
     public Vector3 vector3;
+    public Camera camera2;
+    public Camera camera3;
 
     void Start()
     {
         // 1-й елемент:
-        camera1.fieldOfView = 100; // «м≥нити FOV
-        camera1.backgroundColor = Color.black; // «м≥нити background
+        camera1.fieldOfView = 80; 
+        camera1.backgroundColor = Color.black; 
 
-        cubeTransform.localScale = new Vector3(1, 10, 1); // «м≥нити розм≥р куба
-        cubeTransform.position = new Vector3(0, 0, 0); // «м≥нити позиц≥ю куба
+        cubeTransform.localScale = new Vector3(1, 1, 1); 
+        cubeTransform.position = new Vector3(0, 0, 0);
+
+        // 2-й елемент:
+        light2.intensity = 1; 
+        light2.color = Color.white; 
+
+        vector3 = new Vector3(20, 24.77f, 89.871f); 
+
+        camera1.enabled = false; 
+        camera2.enabled = false; 
+        camera3.enabled = true; 
     }
 }
